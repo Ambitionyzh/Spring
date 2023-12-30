e3d8c0f9fb4a7da8c9e91ff93d11f11d1186f814
wuhuqifei <1214086027@qq.com>
2023-12-30 09:32:23 +0800

    V1

---
+++ spring6_001_revelation/src/main/java/com/yongzh/spring6_001_revelation/Spring6001RevelationApplication.java
@@ -0,0 +1,13 @@
+package com.yongzh.spring6_001_revelation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6001RevelationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6001RevelationApplication.class, args);
+    }
+
+}
---
+++ spring6_001_revelation/src/test/java/com/yongzh/spring6_001_revelation/Spring6001RevelationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yongzh.spring6_001_revelation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6001RevelationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
